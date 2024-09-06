fcd97ecf2d7bf446364864085ef203cf7f005e10
Michael Stiven Restrepo Alvarez <michael.restrepo@puntoscolombia.com>
2024-09-06 11:02:26 -0500

    refactor: remove commonjs plugin from rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import litcss from "rollup-plugin-lit-css";
 import terser from "@rollup/plugin-terser";
@@ -14,5 +13,5 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [resolve(), commonjs(), typescript(), litcss(), terser()],
+  plugins: [resolve(), typescript(), litcss(), terser()],
 };
