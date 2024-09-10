f552d7e8102028460381104ef42725b7679ce95b
Michael Stiven Restrepo Alvarez <michael.restrepo@puntoscolombia.com>
2024-09-10 15:28:47 -0500

    chore: use a click action passed to the component my-component

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,23 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import litcss from "rollup-plugin-lit-css";
 import terser from "@rollup/plugin-terser";
+import glob from "glob";
+import path from "path";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      dir: "dist",
-      format: "esm",
-      entryFileNames: "[name].js",
-      sourcemap: true,
-    },
-  ],
-  plugins: [resolve(), typescript(), litcss(), terser()],
-};
+const components = glob.sync("src/components/**/*.ts");
+
+export default components.map((inputFile) => {
+  const fileName = path.basename(inputFile, path.extname(inputFile));
+  return {
+    input: inputFile,
+    output: [
+      {
+        dir: "dist",
+        format: "esm",
+        entryFileNames: `${fileName}.js`,
+        sourcemap: true,
+      },
+    ],
+    plugins: [resolve(), typescript(), litcss(), terser()],
+  };
+});
--- src/components/my-component.ts
+++ src/components/my-component.ts
@@ -11,9 +11,34 @@ export class MyComponent extends LitElement {
     }
   `;
 
-  @property() name = "Somebody";
+  @property({ type: Number }) number1 = 0;
+  @property({ type: Number }) number2 = 0;
+  @property({ type: Number }) sum = 0;
+  @property({ attribute: false }) handleOnSumNumber: (sum: number) => void =
+    () => {};
+
+  private sumNumbers() {
+    this.sum = this.number1 + this.number2;
+    if (this.handleOnSumNumber) {
+      this.handleOnSumNumber(this.sum);
+    }
+  }
+
+  override updated(changedProperties) {
+    if (changedProperties.has("handleOnSumNumber")) {
+      const handlerName = this.getAttribute("handleOnSumNumber");
+      if (handlerName && typeof window[handlerName] === "function") {
+        this.handleOnSumNumber = window[handlerName];
+      }
+    }
+  }
 
   override render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`
+      <p>Number 1: ${this.number1}</p>
+      <p>Number 2: ${this.number2}</p>
+      <p>Sum: ${this.sum}</p>
+      <button @click="${this.sumNumbers}">Sum Numbers</button>
+    `;
   }
 }
